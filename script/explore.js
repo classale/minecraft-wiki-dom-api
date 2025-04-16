f2b3c0913fcd8075397919dde12e3717d83c9b1e
classale <99489927+classale@users.noreply.github.com>
2025-04-16 11:53:42 +0200

    feat(app): fix things idk I forgor

--- script/explore.js
+++ script/explore.js
@@ -1,18 +1,29 @@
 const searchForm = document.getElementById("search-form")
+const results = document.querySelector(".results")
 
 const API_URL = "http://192.168.1.15:3000/v1"
 
+function createCard(el) {
+    const div = document.createElement("div")
+    div.appendChild(Object.assign(document.createElement("p"), {innerHTML: el.name, className: "entity-name"}));
+    div.appendChild(Object.assign(document.createElement("img"), {src: el.image}));
+    //div.appendChild(Object.assign(document.createElement("pre"), {innerHTML: JSON.stringify(el)}));
+
+    return div;
+}
+
 async function updateSearchField(searchForm) {
     const query = Array.from(new FormData(searchForm).entries().filter(([_, value]) => !!value).map(([key, value]) => `${key}=${value}`)).join("&");
-    
-    console.log(`${API_URL}/entities?${query}`)
+    const ans = await fetch(`${API_URL}/entities?${query}`);
+    const json = await ans.json()
+    for(let el of json) {
+        results.appendChild(createCard(el))
+    }
 }
 
 searchForm.addEventListener("submit", e => {
     e.preventDefault();
     updateSearchField(searchForm)
 })
-
-
 
 updateSearchField(searchForm)
